34dad31ff8c28c37af9717a7169a5afa86178801
Nimisha Mishra <n3mishra@ucsd.edu>
2026-05-16 15:36:11 -0700

    -

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // ── Mapbox setup ─────────────────────────────────────────────────────────────
-mapboxgl.accessToken = 'pk.eyJ1IjoibmltaXNoYW1pc2hyYSIsImEiOiJjbXA4d3pqdWcwOWFjMnJwdHJjZWk1Z2tyIn0.9KR1Asa16cxl53dVkz-gZw';
+mapboxgl.accessToken = 'pk.eyJ1IjoibmltaXNoYW1pc2hyYSIsImEiOiJjbXA4eGI0ZGgwbzVjMnBwdmp1OTMxNHAyIn0.QtiHbKbixCEHl7HT3YqcOg';
 
 const map = new mapboxgl.Map({
   container: 'map',
